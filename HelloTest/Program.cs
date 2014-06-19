f5e7cb0c2bd8eedb7f0036f253025027b55169a6
Gary Tsai <gary.m.tsai@gmail.com>
2014-06-19 13:32:01 -0700

    First commit

---
+++ HelloTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
